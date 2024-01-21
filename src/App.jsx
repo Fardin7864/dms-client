264920b0424c01e2b2f197818699a46148a413e0
Fardin7864 <fardinhasan1888@gmail.com>
2024-01-21 12:25:46 +0600

    create error page , add navbar , design and update the drawer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { NavLink, Outlet } from "react-router-dom";
+import Navbar from "./common/navbar/Navbar";
+import { GoHome } from "react-icons/go";
+import { AiFillFileText, AiFillTags ,AiTwotoneWallet,AiOutlineUsergroupAdd,AiFillDollarCircle, AiFillExclamationCircle, AiOutlineExclamation } from "react-icons/ai";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+        {/* Page content here */}
+        <Navbar/>
+        <Outlet/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-2"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        > </label>
+        <ul className="menu p-4 w-80 min-h-full bg-[#030637] text-base-100">
+          {/* Sidebar content here */}
+          <li className="flex flex-row items-center"><img src="/public/logo.png" alt=""  className="w-20 "/><h4 className=" text-2xl font-bold">VELOCI</h4></li>
+          <li>
+            <NavLink to={'/'}><GoHome className=" text-2xl font-bold"/><span className=" text-md font-semibold">Dashboard</span></NavLink>
+          </li>
+          <li>
+            <NavLink to={'/sell'}><AiFillTags className=" text-2xl font-bold"/><span className=" text-md font-semibold">Sell</span></NavLink>
+          </li>
+          <li>
+            <NavLink to={'/return'}><AiOutlineExclamation  className=" text-2xl font-bold"/><span className=" text-md font-semibold">Return</span></NavLink>
+          </li>
+          <li>
+            <NavLink to={'/orders'}><AiFillFileText className=" text-2xl font-bold"/><span className=" text-md font-semibold">Orders</span></NavLink>
+          </li>
+          <li>
+            <NavLink to={'/stock'}><AiTwotoneWallet  className=" text-2xl font-bold"/><span className=" text-md font-semibold">Stock</span></NavLink>
+          </li>
+          <li>
+            <NavLink to={'/damages'}><AiFillExclamationCircle  className=" text-2xl font-bold"/><span className=" text-md font-semibold">Damages</span></NavLink>
+          </li>
+          <li className="flex flex-row items-center"><h4 className=" text-xl font-bold">EMPLOYEES</h4></li>
+          <li>
+            <NavLink to={'/empployee'}><AiOutlineUsergroupAdd  className=" text-2xl font-bold"/><span className=" text-md font-semibold">Epmployee List</span></NavLink>
+          </li>
+          <li>
+            <NavLink to={'/salary'}><AiFillDollarCircle  className=" text-2xl font-bold"/><span className=" text-md font-semibold">Salary Sheet</span></NavLink>
+          </li>
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div className=" border-4">
+            <h2>This is home!</h2>
+        </div>
+    );
+};
+
+export default Home;
